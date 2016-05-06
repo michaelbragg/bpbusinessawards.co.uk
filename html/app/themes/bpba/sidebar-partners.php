ecad47eb01d6d9c2e79c6a173360a3cf9ca77235
Michael Bragg <michael@michaelbragg.net>
2016-05-06 09:19:05 +0100

    Fix partners bar query

--- html/app/themes/bpba/sidebar-partners.php
+++ html/app/themes/bpba/sidebar-partners.php
@@ -8,68 +8,68 @@
  */
 
 $associate_array = new WP_Query(
-  array(
-  	'post_type'		=> 'ctba-partners',
+	array(
+		'post_type'		=> 'ba-partners',
 		'post_status' => array(
 			'publish',
 		),
 		'nopaging'    => true,
-    'orderby'   => 'title',
-    'order'     => 'ASC',
-    'tax_query' => array(
-      array(
-        'taxonomy' => 'partner-levels',
-        'field'    => 'slug',
-        'terms'    => array( 'associate' ),
-      ),
-    ),
-  )
+		'orderby'   => 'title',
+		'order'     => 'ASC',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'partner-levels',
+				'field'    => 'slug',
+				'terms'    => array( 'associate' ),
+			),
+		),
+	)
 );
 
 $supporter_array = new WP_Query(
-  array(
-  	'post_type'		=> 'ctba-partners',
+	array(
+		'post_type'		=> 'ba-partners',
 		'post_status' => array(
 			'publish',
 		),
 		'nopaging'    => true,
-    'orderby'   => 'title',
-    'order'     => 'ASC',
-    'tax_query' => array(
-      array(
-        'taxonomy' => 'partner-levels',
-        'field'    => 'slug',
-        'terms'    => array( 'supporter' ),
-      ),
-    ),
-  )
+		'orderby'   => 'title',
+		'order'     => 'ASC',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'partner-levels',
+				'field'    => 'slug',
+				'terms'    => array( 'supporter' ),
+			),
+		),
+	)
 );
 
 ?>
 
 <section class="wrapper partners-bar box__large c cf">
 
-  <div class="wrapper__sub">
+	<div class="wrapper__sub">
 
 <?php if ( $associate_array->have_posts() ) : ?>
-    <section class="grid ss1-ss4 ms1-6 ls1-ls4">
+		<section class="grid ss1-ss4 ms1-6 ls1-ls4">
 
 	<!-- pagination here -->
-  <h3 class="gamma heading--main">In association with:</h3>
+	<h3 class="gamma heading--main">In association with:</h3>
 
 	<!-- the loop -->
 	<?php while ( $associate_array->have_posts() ) : $associate_array->the_post(); ?>
-    <?php if ( has_post_thumbnail() ) : ?>
-    <a href="<?php echo esc_url( get_permalink() ); ?>">
-      <?php the_post_thumbnail(
-    	'logo-partner',
-    	array(
-    		'class' => 'image__responsive'
-    	)
-    ); ?>
-    </a>
-	  <?php endif; ?>
-    </a>
+		<?php if ( has_post_thumbnail() ) : ?>
+		<a href="<?php echo esc_url( get_permalink() ); ?>">
+			<?php the_post_thumbnail(
+				'logo-partner',
+				array(
+					'class' => 'image__responsive',
+				)
+			); ?>
+		</a>
+		<?php endif; ?>
+		</a>
 	<?php endwhile; ?>
 	<!-- end of the loop -->
 
@@ -81,29 +81,29 @@ $supporter_array = new WP_Query(
 <?php endif; ?>
 
 <?php if ( $supporter_array->have_posts() ) : ?>
-    <section class="ss1-ss4 ms1-ms6 ls5-ls12">
-      <h3 class="gamma heading--main">Supported by:</h3>
-      <ul class="list gallery cf">
+		<section class="ss1-ss4 ms1-ms6 ls5-ls12">
+			<h3 class="gamma heading--main">Supported by:</h3>
+			<ul class="list gallery cf">
 
-      <?php while ( $supporter_array->have_posts() ) : $supporter_array->the_post(); ?>
-        <li class="gallery__item">
-			    <?php if ( has_post_thumbnail() ) : ?>
-			    <a href="<?php echo esc_url( get_permalink() ); ?>">
-			      <?php the_post_thumbnail(
-			    	'logo-partner',
-			    	array(
-			    		'class' => 'image__responsive'
-			    	)
-			    ); ?>
-			    </a>
-				  <?php endif; ?>
-        </li>
+			<?php while ( $supporter_array->have_posts() ) : $supporter_array->the_post(); ?>
+				<li class="gallery__item">
+					<?php if ( has_post_thumbnail() ) : ?>
+					<a href="<?php echo esc_url( get_permalink() ); ?>">
+					<?php the_post_thumbnail(
+						'logo-partner',
+						array(
+							'class' => 'image__responsive',
+						)
+					); ?>
+					</a>
+					<?php endif; ?>
+				</li>
 	<?php endwhile; ?>
 
-      </ul>
+			</ul>
 
-    </section>
+		</section>
 <?php endif; ?>
-  </div><!--/ wrapper__sub  -->
+	</div><!--/ wrapper__sub  -->
 
 </section>

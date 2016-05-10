f8120379c361761c858713392c5995a6a88f9b01
Michael Bragg <michael@michaelbragg.net>
2016-05-10 13:15:08 +0100

    Add standard development

---
+++ config/environments/development.php
@@ -0,0 +1,21 @@
+<?php
+/* Development */
+
+// Enable WP_DEBUG mode
+define( 'WP_DEBUG', true );
+
+// Enable Debug logging to the /wp-content/debug.log file
+define( 'WP_DEBUG_LOG', true );
+
+// Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
+define( 'SCRIPT_DEBUG', false );
+
+// Save the database queries to an array that can be outputted
+define( 'SAVEQUERIES', true );
+
+// Configure proxy Server
+//define( 'WP_PROXY_HOST', 'proxy' );
+//define( 'WP_PROXY_PORT', '3128' );
+//define( 'WP_PROXY_USERNAME', '' );
+//define( 'WP_PROXY_PASSWORD', '' );
+//define( 'WP_PROXY_BYPASS_HOSTS', 'localhost' );
